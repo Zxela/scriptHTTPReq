62305b1e15a0ccf568f829bbc3d3b71b4c1d4bf8
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-07-09 18:47:07 +0000

    added string reverser

--- scriptHTTPReq/print_reverse.js
+++ scriptHTTPReq/print_reverse.js
@@ -3,3 +3,14 @@ var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step4.html'
 };
+function printReverse (html) {
+    var splitHTML = html.split("");
+    var reverseHTML = splitHTML.reverse();
+    var joinHTML = reverseHTML.join("")
+    console.log(joinHTML)
+    }
+// function printReverse (html) {
+//     console.log(html.toLowerCase());
+// };
+getHTML(requestOptions, printReverse);
+
