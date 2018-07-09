50dfd93b9882347eb1640357bd82ca1a3c6710e1
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-07-09 18:02:50 +0000

    created http-func module to handle getHTML

--- scriptHTTPReq/http-func.js
+++ scriptHTTPReq/http-func.js
@@ -1,6 +1,5 @@
-var https = require('https');
-
-function getHTML (options, callback) {
+module.exports = function getHTML (options, callback) {
+    var https = require('https');
     var newData = "";
     https.get(options, function (response) {
    // set encoding to utf-8
@@ -8,7 +7,7 @@ function getHTML (options, callback) {
    //cb invoken when chunk recieved
         response.on('data', function (data) {
             newData += data;
-     // console.log('data = ', data);
+    // console.log('data = ', data);
      // console.log(arr)
         });
    //cb invoked on chunk end
@@ -17,15 +16,12 @@ function getHTML (options, callback) {
         });
     });
 }
-
 function printHTML (html) {
-    console.log(html);
-}
+        console.log(html);
+    }
 
 var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step4.html'
 };
-
-getHTML(requestOptions, printHTML);
 
