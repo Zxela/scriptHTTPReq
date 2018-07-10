d18abeb02c2b6ad19e786a81b45f557702dd36df
Your Name <alex.zvaniga@gmail.com>
2018-07-10 11:50:10 -0400

    returns html response

--- w2d2/urlShortener/express_server.js
+++ w2d2/urlShortener/express_server.js
@@ -10,6 +10,14 @@ const urlDatabase = {
 app.get("/", (req, res) => {
     res.end("Hello!");
 });
+//added response for urls.json
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+});
+//route to add html to response
+app.get("/hello", (req, res) => {
+    res.end ("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
